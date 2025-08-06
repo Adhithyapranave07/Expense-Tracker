3edce6f2b332a68cc730a204557c4812dbf30d14
Adhithya Pranave <151059501+Adhithyapranave07@users.noreply.github.com>
2025-08-06 06:01:29 +0530

    Update DBConnection.java
    Update privated credintials !

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/expense_db"; // Your DB name
+    private static final String URL = "jdbc:mysql://localhost:3306/YOUR_DB_NAME"; // Your DB name
     private static final String USER = "root";  // Change if needed
-    private static final String PASSWORD = "Adhithya_07";  // Change if you set a password
+    private static final String PASSWORD = "";  // Change if you set a password
 
     public static Connection getConnection() {
         try {
